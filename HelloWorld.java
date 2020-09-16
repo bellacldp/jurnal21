24c3ba484b5486bf7adae61e8ceef4b62a52c0ce
bellacldp <bellacldp@gmail.com>
2020-09-16 16:05:10 +0700

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld { 
+        public static void main (String[] args) { 
+	        System.out.println("Nama : Bella Claudia Pertiwi");
+		    System.out.println("Pelajaran : Implementasi Algoritma"); 
+		    System.out.println("Kelas : D3IF 44-04");
+	     }
+	 }
